cb1b66525e65b6880c0d82e1a5bc27ec4185e94e
Ali Ashraf 777 <aliiashraf777@gmail.com>
2026-04-11 01:30:42 +0500

    feat(Budget): add budget section

--- src/components/Budget/Budget.jsx
+++ src/components/Budget/Budget.jsx
@@ -1,7 +1,7 @@
 import './Budget.css';
 import { iconsImgs } from '../../utils/images';
 import { budget } from '../../data/data';
-
+ 
 const Budget = () => {
     return (
         <div className='grid-two__item grid-common grid-c4'>
